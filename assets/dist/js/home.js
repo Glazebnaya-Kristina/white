21298950dfd49043d9ee34e00fb30f5201dd31cc
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2019-12-17 19:35:14 +0200

    Верстка и стили для секции team. Для блока info-content добавила модификатор Стили для блока button-arrow.

--- assets/dist/js/home.js
+++ assets/dist/js/home.js
@@ -1,8 +1,31 @@
 $(document).ready(function () {
-   // $('.header__slider').slick({
-   //    arrows: false,
-   //    dots: true
-   // });
+   $('.team__carousel-nav').slick({
+      infinite: true,
+      slidesToShow: 2,
+      focusOnSelect: true,
+      prevArrow: '<button type="button" class="button-arrow team__arrow team__arrow--left"><i class="icon-right-arrow"></i></button>',
+      nextArrow: '<button type="button" class="button-arrow team__arrow team__arrow--right"><i class="icon-right-arrow"></i></button>',
+      dots: false,
+      asNavFor: '.team__carousel, .team__carousel-description'
+   });
+
+   $('.team__carousel').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      swipe: false,
+      fade: true,
+      asNavFor: '.team__carousel-nav'
+   });
+
+   $('.team__carousel-description').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      swipe: false,
+      fade: true,
+      asNavFor: '.team__carousel-nav'
+   });
 });
 
 
