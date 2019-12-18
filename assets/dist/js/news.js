0f1abed1067c864bfcbed92fa46878e7e579849e
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2019-12-18 11:42:55 +0200

    Изменен скрипт загрузки новостей.

--- assets/dist/js/news.js
+++ assets/dist/js/news.js
@@ -3,8 +3,5 @@ $(document).ready(function () {
    $("#loadMore").on('click', function (e) {
       e.preventDefault();
       $(".new__item:hidden").slice(0, 3).fadeIn('slow');
-      $('html,body').animate({
-         scrollTop: $(this).offset().top
-      }, 500);
    });
 });
