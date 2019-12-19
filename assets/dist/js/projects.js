1af12ab1d227107d8bdfa1b6d5d273dffee2ba67
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2019-12-19 11:41:22 +0200

    Разметка и стили для секции reviews. Подправлены стили секции gallery под пиксель перфект.

--- assets/dist/js/projects.js
+++ assets/dist/js/projects.js
@@ -4,4 +4,33 @@ $(document).ready(function () {
       e.preventDefault();
       $(".gallery__item:hidden").slice(0, 3).fadeIn('slow');
    });
+
+   $('.reviews__carousel').slick({
+      infinite: true,
+      slidesToShow: 4,
+      prevArrow: '<button type="button" class="button-arrow reviews__arrow reviews__arrow--left"><i class="icon-arrow"></i></button>',
+      nextArrow: '<button type="button" class="button-arrow reviews__arrow reviews__arrow--right"><i class="icon-arrow"></i></button>',
+      dots: false,
+      responsive: [
+         {
+            breakpoint: 1429,
+            settings: {
+               slidesToShow: 3,
+            }
+         },
+         {
+            breakpoint: 1023,
+            settings: {
+               slidesToShow: 2,
+            }
+         },
+         {
+            breakpoint: 767,
+            settings: {
+               slidesToShow: 1,
+            }
+         }
+      ]
+   });
+
 });
