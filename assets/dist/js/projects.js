7904170371fa63dd9054b741aedf979b6cd379f9
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2019-12-19 20:27:17 +0200

    Разметка и стили для секции customers.

--- assets/dist/js/projects.js
+++ assets/dist/js/projects.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
       $(".gallery__item:hidden").slice(0, 3).fadeIn('slow');
    });
 
+
    $('.reviews__carousel').slick({
       infinite: true,
       slidesToShow: 4,
@@ -32,5 +33,47 @@ $(document).ready(function () {
          }
       ]
    });
+
+   var customersCarousel = $('.customers__carousel');
+
+   customersCarousel.on("init", function (event, slick) {
+      $(".step-count").html(parseInt(slick.currentSlide + 1) + '<span> / ' + slick.slideCount + '</span>');
+   });
+
+   customersCarousel.on("afterChange", function (event, slick, currentSlide) {
+      $(".step-count").html(parseInt(slick.currentSlide + 1) + '<span> / ' + slick.slideCount + '</span>');
+   });
+
+
+   customersCarousel.slick({
+      infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      rows: 3,
+      nextArrow: document.querySelector('.customers__arrow--right'),
+      prevArrow: document.querySelector('.customers__arrow--left'),
+      dots: false,
+      mobileFirst: true,
+      responsive: [
+         {
+            breakpoint: 768,
+            settings: {
+               slidesToShow: 4,
+               slidesToScroll: 1,
+            }
+         },
+         {
+            breakpoint: 1024,
+            settings: "unslick"
+         }
+      ]
+   });
+
+   // $(".customers__item").slice(0, 18).show();
+   // $("#loadMoreCustomers").on('click', function (e) {
+   //    e.preventDefault();
+   //    $(".customers__item:hidden").slice(0, 6).fadeIn('slow');
+   // });
+
 
 });
